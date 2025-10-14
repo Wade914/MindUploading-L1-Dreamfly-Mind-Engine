a8de01d24a23d4b691c43723dd56484b9985e2b7
nwllm项目2外部开发02 <nanwang02@cecw.net>
2025-10-14 16:50:50 +0800

    feat: 修改proxy配置

--- vite.config.js
+++ vite.config.js
@@ -29,13 +29,12 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 5173,
       host: '0.0.0.0', // ← 关键：允许外部访问
-      // 注释掉proxy配置，因为已经有Nginx反向代理了
-      // proxy: {
-      //   '/api': {
-      //     target: isProd ? 'http://8.129.25.16:8000' : 'http://localhost:8000',
-      //     changeOrigin: true,
-      //   }
-      // }
+      proxy: {
+        '/api': {
+          target: isProd ? 'http://127.0.0.1:8000' : 'http://localhost:8000',
+          changeOrigin: true,
+        }
+      }
     }
   }
 })
